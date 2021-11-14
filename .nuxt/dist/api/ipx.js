de9d408bd1d7bf94e8f9ffb363e1b338e64d415b
Zafar6699 <webzafar6699@gmail.com>
2021-11-14 21:59:42 +0500

    asd

---
+++ .nuxt/dist/api/ipx.js
@@ -0,0 +1,3 @@
+import { createIPX, createIPXMiddleware } from "ipx";
+const ipx = createIPX({"dir":"C:/Users/ZAFAR_0210/OneDrive/Desktop/work/tujjor/static","domains":[],"sharp":{},"alias":{}});
+export default createIPXMiddleware(ipx);
