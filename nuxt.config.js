c4f7a0bdec932d86cc77a2deb19ccb0ecc4a6401
Zafar6699 <webzafar6699@gmail.com>
2021-11-14 19:07:53 +0500

    ad

--- nuxt.config.js
+++ nuxt.config.js
@@ -190,6 +190,9 @@ export default {
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {
+        babel: {
+            compact: true
+        },
         transpile: ["vue-slick"],
         vendor: ["vue-slider-component", "star-rating"]
     }
