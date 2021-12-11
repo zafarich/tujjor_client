3f74c1e99e99f04410c6c8dba50a2173dc1d39d0
Zafar6699 <webzafar6699@gmail.com>
2021-12-11 00:02:59 +0000

    asd

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,25 +1,25 @@
-export { default as AboutUs } from '../..\\components\\AboutUs.vue'
-export { default as BannerOnMainPage } from '../..\\components\\BannerOnMainPage.vue'
-export { default as Brands } from '../..\\components\\brands.vue'
-export { default as CatalogMagazinesMainPage } from '../..\\components\\CatalogMagazinesMainPage.vue'
-export { default as CatalogPage } from '../..\\components\\CatalogPage.vue'
-export { default as Footer } from '../..\\components\\Footer.vue'
-export { default as Header } from '../..\\components\\Header.vue'
-export { default as HeaderMainPage } from '../..\\components\\HeaderMainPage.vue'
-export { default as LogoRectangle } from '../..\\components\\LogoRectangle.vue'
-export { default as MagazineDescription } from '../..\\components\\MagazineDescription.vue'
-export { default as News } from '../..\\components\\news.vue'
-export { default as OnSaleItem } from '../..\\components\\OnSaleItem.vue'
-export { default as OtherItems } from '../..\\components\\OtherItems.vue'
-export { default as PopularItem } from '../..\\components\\PopularItem.vue'
-export { default as ProductCard } from '../..\\components\\ProductCard.vue'
-export { default as ProductLike } from '../..\\components\\ProductLike.vue'
-export { default as FaqQuestions } from '../..\\components\\FAQ\\FaqQuestions.vue'
-export { default as ModalsDangerMessage } from '../..\\components\\Modals\\DangerMessage.vue'
-export { default as ModalsSuccessModal } from '../..\\components\\Modals\\SuccessModal.vue'
-export { default as ModalsWarningMessage } from '../..\\components\\Modals\\WarningMessage.vue'
-export { default as UIBaseLoading } from '../..\\components\\UI\\BaseLoading.vue'
-export { default as UILoadingOnBlocks } from '../..\\components\\UI\\LoadingOnBlocks.vue'
+export const AboutUs = () => import('../..\\components\\AboutUs.vue' /* webpackChunkName: "components/about-us" */).then(c => wrapFunctional(c.default || c))
+export const BannerOnMainPage = () => import('../..\\components\\BannerOnMainPage.vue' /* webpackChunkName: "components/banner-on-main-page" */).then(c => wrapFunctional(c.default || c))
+export const Brands = () => import('../..\\components\\brands.vue' /* webpackChunkName: "components/brands" */).then(c => wrapFunctional(c.default || c))
+export const CatalogMagazinesMainPage = () => import('../..\\components\\CatalogMagazinesMainPage.vue' /* webpackChunkName: "components/catalog-magazines-main-page" */).then(c => wrapFunctional(c.default || c))
+export const CatalogPage = () => import('../..\\components\\CatalogPage.vue' /* webpackChunkName: "components/catalog-page" */).then(c => wrapFunctional(c.default || c))
+export const Footer = () => import('../..\\components\\Footer.vue' /* webpackChunkName: "components/footer" */).then(c => wrapFunctional(c.default || c))
+export const Header = () => import('../..\\components\\Header.vue' /* webpackChunkName: "components/header" */).then(c => wrapFunctional(c.default || c))
+export const HeaderMainPage = () => import('../..\\components\\HeaderMainPage.vue' /* webpackChunkName: "components/header-main-page" */).then(c => wrapFunctional(c.default || c))
+export const LogoRectangle = () => import('../..\\components\\LogoRectangle.vue' /* webpackChunkName: "components/logo-rectangle" */).then(c => wrapFunctional(c.default || c))
+export const MagazineDescription = () => import('../..\\components\\MagazineDescription.vue' /* webpackChunkName: "components/magazine-description" */).then(c => wrapFunctional(c.default || c))
+export const News = () => import('../..\\components\\news.vue' /* webpackChunkName: "components/news" */).then(c => wrapFunctional(c.default || c))
+export const OnSaleItem = () => import('../..\\components\\OnSaleItem.vue' /* webpackChunkName: "components/on-sale-item" */).then(c => wrapFunctional(c.default || c))
+export const OtherItems = () => import('../..\\components\\OtherItems.vue' /* webpackChunkName: "components/other-items" */).then(c => wrapFunctional(c.default || c))
+export const PopularItem = () => import('../..\\components\\PopularItem.vue' /* webpackChunkName: "components/popular-item" */).then(c => wrapFunctional(c.default || c))
+export const ProductCard = () => import('../..\\components\\ProductCard.vue' /* webpackChunkName: "components/product-card" */).then(c => wrapFunctional(c.default || c))
+export const ProductLike = () => import('../..\\components\\ProductLike.vue' /* webpackChunkName: "components/product-like" */).then(c => wrapFunctional(c.default || c))
+export const FaqQuestions = () => import('../..\\components\\FAQ\\FaqQuestions.vue' /* webpackChunkName: "components/faq-questions" */).then(c => wrapFunctional(c.default || c))
+export const ModalsDangerMessage = () => import('../..\\components\\Modals\\DangerMessage.vue' /* webpackChunkName: "components/modals-danger-message" */).then(c => wrapFunctional(c.default || c))
+export const ModalsSuccessModal = () => import('../..\\components\\Modals\\SuccessModal.vue' /* webpackChunkName: "components/modals-success-modal" */).then(c => wrapFunctional(c.default || c))
+export const ModalsWarningMessage = () => import('../..\\components\\Modals\\WarningMessage.vue' /* webpackChunkName: "components/modals-warning-message" */).then(c => wrapFunctional(c.default || c))
+export const UIBaseLoading = () => import('../..\\components\\UI\\BaseLoading.vue' /* webpackChunkName: "components/u-i-base-loading" */).then(c => wrapFunctional(c.default || c))
+export const UILoadingOnBlocks = () => import('../..\\components\\UI\\LoadingOnBlocks.vue' /* webpackChunkName: "components/u-i-loading-on-blocks" */).then(c => wrapFunctional(c.default || c))
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
---
+++ .nuxt/dist/api/ipx.js
@@ -0,0 +1,3 @@
+import { createIPX, createIPXMiddleware } from "ipx";
+const ipx = createIPX({"dir":"C:/Users/ZAFAR_0210/OneDrive/Desktop/tujjor_test/static","domains":[],"sharp":{},"alias":{}});
+export default createIPXMiddleware(ipx);
